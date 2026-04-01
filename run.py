a7b72dc44ab58b522086f56b36480db4b986270c
Preetam Verma <pritamv255@gmail.com>
2026-04-01 11:22:24 +0530

    fix run.py crash

--- run.py
+++ run.py
@@ -21,6 +21,7 @@
 
 
 
+import os
 from flask import Flask
 from routes.main import main
 
@@ -28,4 +29,5 @@ app = Flask(__name__)
 app.register_blueprint(main)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
