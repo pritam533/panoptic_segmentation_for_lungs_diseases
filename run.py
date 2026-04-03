9983076c85d56439cd802731cbfd0c4cc951e742
Preetam Verma <pritamv225@gmail.com>
2026-04-03 13:57:11 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,8 @@ app = Flask(__name__,
 
 app.register_blueprint(main)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))  # 🔥 IMPORTANT
+    app.run(host="0.0.0.0", port=port)
 
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 10000))
