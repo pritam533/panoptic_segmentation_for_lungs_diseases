a2303ea16245c76afebe427488fb54c2e46ec2ef
Preetam Verma <pritamv225@gmail.com>
2026-04-03 14:35:48 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes.main import main
+# from routes.main import main
 import os
 app = Flask(__name__, 
             static_folder='app/static',  
@@ -36,9 +36,9 @@ if __name__ == "__main__":
 #             template_folder='frontend')
 # app.register_blueprint(main)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host="0.0.0.0", port=port)
 
 
 
